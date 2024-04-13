ad125b0412f09ca98c52e38ff364bed4cd5975ee
小新 <3303665926@qq.com>
2024-04-13 16:25:47 +0800

    提交代码

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -8,7 +8,39 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>Home</div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+/* 接着针对您的布局设置为100% */
+.common-layout {
+  height: 100%;
+
+  .el-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .el-header {
+    // 如果你想要一个固定高度的 header，这里指定
+    // 否则如果是动态高度，则不需要设置
+  }
+
+  .el-main {
+    flex: 1; // 使用flex来填充剩余空间
+    overflow: auto; // 如果内容超出，允许滚动
+  }
+  .el-footer{
+    // 如果你想要一个固定高度的 header，这里指定
+    // 否则如果是动态高度，则不需要设置
+  }
+}
+</style>
